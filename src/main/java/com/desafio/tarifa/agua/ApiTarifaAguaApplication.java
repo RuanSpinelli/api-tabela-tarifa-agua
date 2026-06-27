3bd8028cb9a6595dc8bacee8c40df0aeca2c8de1
Ruan Spinelli <ruanspinelli@gmail.com>
2026-06-27 17:42:15 -0300

    Primeiro commit

---
+++ src/main/java/com/desafio/tarifa/agua/ApiTarifaAguaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.tarifa.agua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTarifaAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTarifaAguaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/tarifa/agua/ApiTarifaAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tarifa.agua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarifaAguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
